6d9768d77b1eaa534ac699e84a54b1837a009cd9
bossiel <bossiel@yahoo.fr>
2011-04-28 08:52:52 +0000

    Update 2.x

--- idoubs/Classes/idoubsAppDelegate.h
+++ idoubs/Classes/idoubsAppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  idoubsAppDelegate.h
-//  idoubs
-//
-//  Created by Mamadou DIOP on 4/19/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface idoubsAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
