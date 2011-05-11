82c0595e60d758e94d32149764af537ad7b6eeec
bossiel@yahoo.fr <bossiel@yahoo.fr@98b68a54-b8e4-d874-94f5-50d40778346d>
2011-05-11 10:30:28 +0000

    Update v2.x

--- branches/2.0/ios-ngn-stack/services/impl/NgnSoundService.mm
+++ branches/2.0/ios-ngn-stack/services/impl/NgnSoundService.mm
@@ -1,6 +1,23 @@
 #import "NgnSoundService.h"
 
+#undef TAG
+#define kTAG @"NgnSoundService///: "
+#define TAG kTAG
 
 @implementation NgnSoundService
+
+//
+// INgnBaseService
+//
+
+-(BOOL) start{
+	NgnNSLog(TAG, @"Start()");
+	return YES;
+}
+
+-(BOOL) stop{
+	NgnNSLog(TAG, @"Stop()");
+	return YES;
+}
 
 @end
