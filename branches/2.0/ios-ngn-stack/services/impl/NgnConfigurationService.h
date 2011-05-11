fe8b9111ce31c9bd5632379984579a1e6d8a942b
bossiel@yahoo.fr <bossiel@yahoo.fr@98b68a54-b8e4-d874-94f5-50d40778346d>
2011-05-11 10:37:25 +0000

    Update v2.x

--- branches/2.0/ios-ngn-stack/services/impl/NgnConfigurationService.h
+++ branches/2.0/ios-ngn-stack/services/impl/NgnConfigurationService.h
@@ -1,8 +1,8 @@
 #import <Foundation/Foundation.h>
 
 #import "iOSNgnConfig.h"
-#import "NgnBaseService.h"
-#import "INgnConfigurationService.h"
+#import "services/impl/NgnBaseService.h"
+#import "services/INgnConfigurationService.h"
 
 @interface NgnConfigurationService : NgnBaseService<INgnConfigurationService> {
 	NSUserDefaults* mPrefs;
--- branches/2.0/ios-ngn-stack/services/impl/NgnNetworkService.h
+++ branches/2.0/ios-ngn-stack/services/impl/NgnNetworkService.h
@@ -1,8 +1,8 @@
 #import <Foundation/Foundation.h>
 
 #import "iOSNgnConfig.h"
-#import "NgnBaseService.h"
-#import "INgnNetworkService.h"
+#import "services/impl/NgnBaseService.h"
+#import "services/INgnNetworkService.h"
 
 @interface NgnNetworkService : NgnBaseService <INgnNetworkService>{
 
