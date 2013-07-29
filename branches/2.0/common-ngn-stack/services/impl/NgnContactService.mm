61fd19d325b3c949ea8f930ad10254e84a64d97f
bossiel <bossiel@yahoo.fr>
2013-07-29 01:00:45 +0000

    Add guard arround UIKit.h on OSX

--- branches/2.0/common-ngn-stack/services/impl/NgnContactService.mm
+++ branches/2.0/common-ngn-stack/services/impl/NgnContactService.mm
@@ -18,7 +18,9 @@
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
+#if TARGET_OS_IPHONE
 #import <UIKit/UIKit.h> /* UIDevice */
+#endif
 #import "NgnContactService.h"
 #import "model/NgnContact.h"
 #import "events/NgnContactEventArgs.h"
