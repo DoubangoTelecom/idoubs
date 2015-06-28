19a75a6696547cb5e5259021fbb26055b9ffce38
bossiel@yahoo.fr <bossiel@yahoo.fr@98b68a54-b8e4-d874-94f5-50d40778346d>
2015-06-28 18:23:30 +0000

    Update iOSGLView to add support for delegate, PAR (Pixel Aspect Ratio) and fullscreen

--- branches/2.0/ios-ngn-stack/iOSGLView.h
+++ branches/2.0/ios-ngn-stack/iOSGLView.h
@@ -17,41 +17,38 @@
  * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
-#if TARGET_OS_IPHONE
+#ifndef IOS_NGN_IOS_GLVIEW_H
+#define IOS_NGN_IOS_GLVIEW_H
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import <QuartzCore/QuartzCore.h>
-#include <OpenGLES/ES2/gl.h>
-#include <OpenGLES/ES2/glext.h>
 
-@interface iOSGLView : UIView
-{
-    CAEAGLLayer* _eaglLayer;
-    EAGLContext* _context;
-    CADisplayLink* _displayLink;
-    GLuint _program;
-    GLuint _renderBuffer;
-    GLuint _framebuffer;
-    GLuint _positionSlot;
-    GLuint _texCoordSlot;
-    GLuint _lumaUniform, _chromaUniformU, _chromaUniformV;
-    GLuint _lumaTexture, _chromaTextureU, _chromaTextureV;
-    CGFloat _screenWidth, _screenHeight;
-    GLint _textureWidth, _textureHeight;
-    GLuint _vertexShader, _fragmentShader;
-    uint _bufferWidth, _bufferHeight, _bufferSize;
-    uint8_t* _buffer;
-    GLboolean _animating;
-    GLuint _fps;
+@protocol iOSGLViewDelegate <NSObject>
+@optional
+-(void) glviewAnimationStarted;
+-(void) glviewAnimationStopped;
+-(void) glviewVideoSizeChanged;
+-(void) glviewViewportSizeChanged;
+@end
+
+@interface iOSGLView : UIView {
 }
 
 -(void)setFps:(GLuint)fps;
 -(void)startAnimation;
 -(void)stopAnimation;
 -(void)setOrientation:(UIDeviceOrientation)orientation;
--(void)setBufferYUV:(const uint8_t*)buffer andWidth:(uint)bufferWidth andHeight:(uint)bufferHeight;
-
+-(void)setBufferYUV:(const uint8_t*)buffer width:(uint)bufferWidth height:(uint)bufferHeight;
+-(void)setDelegate:(id<iOSGLViewDelegate>)delegate;
+-(void)setPAR:(int)numerator denominator:(int)denominator;
+-(void)setFullscreen:(BOOL)fullscreen;
+@property(readonly) int viewportX;
+@property(readonly) int viewportY;
+@property(readonly) int viewportWidth;
+@property(readonly) int viewportHeight;
+@property(readonly) int videoWidth;
+@property(readonly) int videoHeight;
+@property(readonly) BOOL animating;
 @end
 
-#endif /* #if TARGET_OS_IPHONE */
+#endif /* IOS_NGN_IOS_GLVIEW_H */
